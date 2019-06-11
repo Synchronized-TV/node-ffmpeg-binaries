a3d20ec71015f7ed00e5fe85d26a5dc69371e4de
Jidé <jide.fr@gmail.com>
2019-06-11 16:18:47 +0200

    Remove ffprobe

--- install.js
+++ install.js
@@ -35,7 +35,7 @@ function callback(res) {
     decompress(buf, 'bin', {
       plugins: process.platform === 'linux' ? [tarxz()] : [unzip()],
       strip: process.platform === 'linux' ? 1 : 2,
-      filter: x => x.path.match(/ffmpeg(\.exe)?$|ffprobe(\.exe)?$/),
+      filter: x => x.path.match(/ffmpeg(\.exe)?$/),
     });
   });
 }
